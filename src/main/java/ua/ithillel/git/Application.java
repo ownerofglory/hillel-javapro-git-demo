8ab389901e49e8c69da3df0fb39782dc1aa136e0
Vlady <vladyslav.v.zasoba@mercedes-benz.com>
2023-10-13 18:45:11 +0200

    Add program entry point

---
+++ src/main/java/ua/ithillel/git/Application.java
@@ -0,0 +1,9 @@
+package ua.ithillel.git;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Application started...");
+
+        System.out.println("Application finished");
+    }
+}
