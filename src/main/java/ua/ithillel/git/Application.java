ac46827c6c7a4c489f03113f0cc5febdd3622692
Vlady <vladyslav.v.zasoba@mercedes-benz.com>
2023-10-13 18:58:07 +0200

    Create service layer
    - add string service
    - imlement string service

--- src/main/java/ua/ithillel/git/Application.java
+++ src/main/java/ua/ithillel/git/Application.java
@@ -1,8 +1,16 @@
 package ua.ithillel.git;
 
+import ua.ithillel.git.service.StringService;
+import ua.ithillel.git.service.StringServiceDefault;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Application started...");
+
+        StringService service = new StringServiceDefault();
+
+        String helloUpperCase = service.upperCase("Hello");
+        System.out.printf("Converted string %s%n", helloUpperCase);
 
         System.out.println("Application finished");
     }
